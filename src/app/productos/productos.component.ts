457fa4a09b6489cca3020d3a7c9142cfc2d2fe3c
Ricardo Ferreira <ricardoferreiran@gmail.com>
2025-10-23 21:47:30 +0200

    pruebas con interfaces y consumo de apis externas

--- src/app/productos/productos.component.ts
+++ src/app/productos/productos.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Producto } from 'src/Models/producto.model';
+
 
 @Component({
   selector: 'app-productos',
   templateUrl: './productos.component.html',
   styleUrls: ['./productos.component.css']
 })
-export class ProductosComponent {
+
+
+export class ProductosComponent implements OnInit{
+  
+
+  url : string = 'https://fakestoreapi.com/products'
+  arrayProductos : Producto[] = []
+
+  constructor(){}
+
+  
+ngOnInit(): void {
+  this.fetchProducts()
+}
+
+async fetchProducts()  {
+    try {
+      const res = await fetch(this.url);
+      const data = await res.json()
+      this.arrayProductos = data
+      console.log(this.arrayProductos)
+    } catch (error) {
+        console.error('Ha ocurrido el siguiente error: ', error)
+    }
+}
 
 }
